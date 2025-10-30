6bf7861f84edd4c5ef2ec123ee52362103901949
Eduardo da Cunha <cunhaxdudu@gmail.com>
2025-10-30 19:50:05 -0300

    first commit

--- ClassesMetodos/obj/Debug/net8.0/ClassesMetodos.AssemblyInfo.cs
+++ ClassesMetodos/obj/Debug/net8.0/ClassesMetodos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassesMetodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a683218670d31038e46507a40253ce8ce6c12870")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassesMetodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassesMetodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
