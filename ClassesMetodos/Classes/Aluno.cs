6dc0e56690e7e15461bf84034939e5fd645ed2db
Eduardo da Cunha <cunhaxdudu@gmail.com>
2025-10-30 22:32:26 -0300

    aula ClassesMetodos 30out

--- ClassesMetodos/Classes/Aluno.cs
+++ ClassesMetodos/Classes/Aluno.cs
@@ -1,10 +1,34 @@
-﻿namespace ClassesMetodos.Classes
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ClassesMetodos.Classes
 {
     public class Aluno
     {
+        public int Indice { get; set; }
         public double Ra { get; set; }
         public string Nome { get; set; }
         public double Nota1bim { get; set; }
         public double Nota2bim { get; set; }
+        public string Turma { get; set; }
+        public double CalcularMedia()
+        {
+            return (Nota1bim + Nota2bim) / 2;
+        }
+
+        public string RetornarSitacao()
+        {
+            if (CalcularMedia() < 4)
+            {
+                return "Reprovado";
+            } 
+            else if (CalcularMedia() >4 && CalcularMedia() <7)
+            {
+                return "De final";
+            }
+            else
+            {
+                return "Aprovado";
+            }
+        }
     }
 }
--- ClassesMetodos/Program.cs
+++ ClassesMetodos/Program.cs
@@ -1,10 +1,72 @@
 ﻿using ClassesMetodos.Classes;
 
-var al1 = RetornarAluno();
-ExibirInfoAluno(al1);
+var listaAlunos = new List<Aluno>();
 
-var al2 = RetornarAluno();
-ExibirInfoAluno(al2);
+/*
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"\n--- Cadastro do {i + 1}° Aluno ---");
+    var aluno = RetornarAluno();
+    aluno.Indice = i +1;
+    listaAlunos.Add(aluno);
+}
+*/
+
+string[] LerArquivo(string arq)
+{
+    try
+    {
+        var linhas = File.ReadAllLines(arq);
+        return linhas;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"\n{DateTime.Now} -Ocorreu um erro inesperado, parando aplicação... {ex}");
+        throw ex;
+    }
+}
+
+Aluno TransformaLinhaAluno(string linha, int indiceLinha)
+{
+    string[] colunas = linha.Split(";");
+    var aluno = new Aluno();
+    if (double.TryParse(colunas[0], out double ra) == false)
+    {
+        Console.WriteLine($"Erro ao ra. Linha{linha}");
+    }
+    aluno.Ra = ra;
+    aluno.Nome = colunas[1];
+    if (double.TryParse(colunas[2], out double n1) == false)
+    {
+        Console.WriteLine($"Erro ao converter nota 1. Linha{linha}");
+    }
+    if (double.TryParse(colunas[3], out double n2) == false)
+    {
+        Console.WriteLine($"Erro ao converter nota 2. Linha{linha}");
+    }
+    aluno.Nota1bim = n1;
+    aluno.Nota2bim = n2;
+    aluno.Turma = colunas[4];
+    aluno.Indice = indiceLinha;
+    return aluno;
+}
+
+var arqv = @"C:\Users\cunha\OneDrive\Documents\Projetos\2ESAN\Programação de Computadores\2° Bimestre\ProgComp2\ArquivoCsv\Teste.csv";
+string[] linhas = LerArquivo(arqv).Skip(1).ToArray();
+
+int indice = 1;
+foreach (var linha in linhas) 
+{
+    var aluno = TransformaLinhaAluno(linha, indice);
+    listaAlunos.Add(aluno);
+    indice++;
+}
+
+foreach (var aluno in listaAlunos)
+{
+    ExibirInfoAluno(aluno);
+}
+
 /*
 //Aluno RetornarAluno()
 //{
@@ -43,37 +105,50 @@ ExibirInfoAluno(al2);
 //}
 */
 
-string RetornaTexto(string msg)
+string RetornarTexto(string msg)
 {
     Console.WriteLine(msg);
     return Console.ReadLine();
 }
 
-double RetornaValor(string msg)
+double RetornarValor(string msg)
 {
     Console.WriteLine(msg);
     double.TryParse(Console.ReadLine(), out double valor);
     return valor;
 }
 
+/*
+string retornarSituacao()
+{
+    if
+    return "";
+}
+*/
+
 Aluno RetornarAluno()
 {
     var aluno = new Aluno();
-    aluno.Ra = RetornaValor("Digite o R.A do Aluno: ");
-    aluno.Nome = RetornaTexto("Digite o Nome do Aluno: ");
-    aluno.Nota1bim = RetornaValor("Digite a Nota do 1° Bim: ");
-    aluno.Nota2bim = RetornaValor("Digite a nota do 2° Bim: ");
+    aluno.Ra = RetornarValor("Digite o R.A do Aluno: ");
+    aluno.Nome = RetornarTexto("Digite o Nome do Aluno: ");
+    aluno.Nota1bim = RetornarValor("Digite a Nota do 1° Bim: ");
+    aluno.Nota2bim = RetornarValor("Digite a nota do 2° Bim: ");
 
     return aluno;
 }
 
 void ExibirInfoAluno(Aluno aluno)
 {
-    Console.WriteLine($"\nInformações do aluno:");
-    Console.WriteLine($"Nome: {aluno.Nome}");
-    Console.WriteLine($"RA: {aluno.Ra}");
-    Console.WriteLine($"Nota 1° Bim: {aluno.Nota1bim}");
-    Console.WriteLine($"Nota 2° Bim: {aluno.Nota2bim}");
+        Console.WriteLine($"\n===Informações do aluno nº{aluno.Indice}===");
+        Console.WriteLine($"Nome: {aluno.Nome}");
+        Console.WriteLine($"RA: {aluno.Ra}");
+        Console.WriteLine($"Nota 1° Bim: {aluno.Nota1bim}");
+        Console.WriteLine($"Nota 2° Bim: {aluno.Nota2bim}");
+        Console.WriteLine($"Média: {aluno.CalcularMedia()}");
+        Console.WriteLine($"Situação: {aluno.RetornarSitacao()}");
+        Console.WriteLine($"Turma: {aluno.Turma}");
 }
+
+
 
 
